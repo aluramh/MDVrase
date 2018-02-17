6fdfc494351a5f71423a03217c47a89cd5f08ad9
aluramh <aluramh@gmail.com>
2018-02-17 12:23:08 -0600

    Polizas and error handling.

--- app.js
+++ app.js
@@ -83,14 +83,15 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
+    console.error(err);
     res.status(err.status || 500);
-    res.render('error');
+    res.send(err);
 });
 
 module.exports = app;
--- models/polizas.js
+++ models/polizas.js
@@ -3,7 +3,19 @@ const { esc } = db;
 
 module.exports = {
   getPolizas: () => new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM polizas WHERE 1';
+    const query = `SELECT
+        polizas.*,
+        carros.*,
+        marcas.nombre_marca,
+        empresas.nombre_empresa
+      FROM carros_polizas
+      LEFT JOIN polizas 
+        ON carros_polizas.num_poliza = polizas.num_poliza
+      LEFT JOIN carros
+        ON carros_polizas.id_carro = carros.id_carro
+      LEFT JOIN marcas ON carros.marca = marcas.id_marca
+      LEFT JOIN empresas ON carros.empresa = empresas.id_empresa
+      WHERE 1`;
 
     db.query(query, null, (err, rows) => {
       if (err) return reject(err);
